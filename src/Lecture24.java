9f47b3da3e6346cb936fe544479e59cb7fdeb66b
Robert Evans <robertevans@robertevansmb.com>
2018-05-25 09:24:21 -0400

    Cover expressions

--- src/Lecture24.java
+++ src/Lecture24.java
@@ -1,5 +1,16 @@
 public class Lecture24 {
     public static void main(String[] args){
+        //Can't use int as a variable it is a reserved word (keyword)
+        //int int=5;
+
+        // expressions
+        // a mile is equal to 1.609344 kilometres
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
 
     }
 }
