7929353d9b20fe6e305dc36a959c88d63c6ff281
Robert Evans <robertevans@robertevansmb.com>
2018-05-25 13:52:31 -0400

    First run of calculateScore()

--- src/Lecture28.java
+++ src/Lecture28.java
@@ -1,9 +1,18 @@
 public class Lecture28 {
     public static void main(String[] args){
-
+        calculateScore();
     }
 
     public static void calculateScore(){
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompeted = 5;
+        int bonus = 100;
 
+        if(gameOver) {
+            int finalScore = score + (levelCompeted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }
