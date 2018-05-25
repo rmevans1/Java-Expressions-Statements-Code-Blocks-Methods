6dc4932041c7ba553157e8b418ed215f330db0c3
Robert Evans <robertevans@robertevansmb.com>
2018-05-25 13:35:07 -0400

    else if statement

--- src/Lecture26.java
+++ src/Lecture26.java
@@ -19,5 +19,15 @@ public class Lecture26 {
         } else {
             System.out.println("Got here");
         }
+
+        //else if
+        score = 4000;
+        if(score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000 but greater than 1000");
+        } else if(score < 1000){
+            System.out.println("Your score was less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
     }
 }
