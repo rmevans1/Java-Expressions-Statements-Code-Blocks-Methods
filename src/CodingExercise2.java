4e250f71144564779fdad875ff2aea0f3a3030b2
Robert Evans <robertevans@robertevansmb.com>
2018-05-29 15:27:18 -0400

    Finish coding exercise 2

--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,5 +1,16 @@
 public class CodingExercise2 {
     public static void main(String[] args){
+        System.out.println(bark(true, 1));
+        System.out.println(bark(false, 2));
+        System.out.println(bark(true, 8));
+        System.out.println(bark(true, -1));
+    }
 
+    public static boolean bark(boolean barking, int hourOfDay){
+        if(barking && (((hourOfDay >= 0) && (hourOfDay < 8)) || ((hourOfDay > 22) &&(hourOfDay<24)))){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
