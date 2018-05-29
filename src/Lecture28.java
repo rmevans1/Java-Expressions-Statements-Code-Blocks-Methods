ffeb6bafd109d41a8f7972107e6b396639cc81c9
Robert Evans <robertevans@robertevansmb.com>
2018-05-29 14:18:36 -0400

    Add parameters to calculateScore()

--- src/Lecture28.java
+++ src/Lecture28.java
@@ -1,16 +1,11 @@
 public class Lecture28 {
     public static void main(String[] args){
-        calculateScore();
+        calculateScore(true, 800, 5,100);
     }
 
-    public static void calculateScore(){
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompeted = 5;
-        int bonus = 100;
-
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         if(gameOver) {
-            int finalScore = score + (levelCompeted * bonus);
+            int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }
