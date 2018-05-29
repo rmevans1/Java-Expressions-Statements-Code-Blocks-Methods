969847264113b5d32f4014ddcac8718789ebb54e
Robert Evans <robertevans@robertevansmb.com>
2018-05-29 14:20:44 -0400

    Add second function call

--- src/Lecture28.java
+++ src/Lecture28.java
@@ -1,6 +1,7 @@
 public class Lecture28 {
     public static void main(String[] args){
         calculateScore(true, 800, 5,100);
+        calculateScore(true, 10000, 8, 200);
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
