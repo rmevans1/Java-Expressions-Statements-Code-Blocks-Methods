fc47d2ce924bf9902b665370c25ce0b88fe22a52
Robert Evans <robertevans@robertevansmb.com>
2018-05-29 14:49:39 -0400

    Finish calculateScore2()

--- src/Lecture29.java
+++ src/Lecture29.java
@@ -1,5 +1,24 @@
 public class Lecture29 {
     public static void main(String[] args){
+        calculateScore(true, 800, 5,100);
+        int highScore = calculateScore2(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
+    }
 
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    public static int calculateScore2(boolean gameOver, int score, int levelCompleted, int bonus){
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            return finalScore;
+        }
+        return -1;
     }
 }
