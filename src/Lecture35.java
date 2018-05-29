ba5fb6615e154673f7ccb3b8b489c3b3c7543009
Robert Evans <robertevans@robertevansmb.com>
2018-05-29 15:42:35 -0400

    Create first method

--- src/Lecture35.java
+++ src/Lecture35.java
@@ -1,5 +1,11 @@
 public class Lecture35 {
     public static void main(String[] args){
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+    }
 
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
     }
 }
