7dc8735bf6c53d58f8b25e6ecfa13177aaf5d49a
Robert Evans <robertevans@robertevansmb.com>
2018-05-30 13:44:40 -0400

    Finish function for area of circle

--- src/CodingExercise4.java
+++ src/CodingExercise4.java
@@ -1,5 +1,15 @@
 public class CodingExercise4 {
     public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
 
+    }
+
+    public static double area(double radius){
+        if(radius < 0) {
+            return -1;
+        }
+
+        return radius * radius * 3.14159;
     }
 }
